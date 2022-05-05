314d771914a81963fe172531c008d239c967eba5
DILTPE - Ty Hsu <B0191@twusr.local>
2022-05-05 15:05:47 +0800

    import team_name from json

--- src/components/search.js
+++ src/components/search.js
@@ -1,41 +1,67 @@
-function main() {
-  return (
-    <form className="mx-5 mt-5 p-3 border">
-      <div className="row g-3 align-items-center mb-3 d-flex justify-content-center">
-        <div className="col-auto">
-          <label for="inputPassword6" class="col-form-label">
-            Team:
-          </label>
-        </div>
-        <div className="col-auto me-5">
-          <select className="form-select" id="inputGroupSelect01">
-            <option selected>All</option>
-            <option value="1">LA Lakers</option>
-            <option value="2">GS Warriors</option>
-            <option value="3">NY Knicks</option>
-          </select>
-        </div>
-        <div className="col-auto">
-          <label for="inputPassword6" class="col-form-label">
-            Keywords:
-          </label>
-        </div>
-        <div className="col-auto">
-          <input
-            type="text"
-            id="inputPassword6"
-            className="form-control"
-            aria-describedby="passwordHelpInline"
-          ></input>
-        </div>
-      </div>
-      <div className="col-auto d-flex justify-content-center">
-        <button type="submit" className="btn btn-lg btn-secondary mt-5">
-          Search
-        </button>
-      </div>
-    </form>
-  );
+import React, { Component } from "react";
+import players from "./players.json";
+
+const key = "team_name";
+const arrayUniqueByKey = [
+  ...new Map(players.map((item) => [item[key], item])).values(),
+];
+
+function SortArray(x, y) {
+  if (x.team_name < y.team_name) {
+    return -1;
+  }
+  if (x.team_name > y.team_name) {
+    return 1;
+  }
+  return 0;
 }
 
-export default main;
+const arrayOrderByTeam = arrayUniqueByKey.sort(SortArray);
+
+class LocalFileRead extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    
+    return (
+      <form className="mx-5 mt-5 p-3 border">
+        <div className="row g-3 align-items-center mb-3 d-flex justify-content-center">
+          <div className="col-auto">
+            <label for="inputPassword6" class="col-form-label">
+              Team:
+            </label>
+          </div>
+          <div className="col-auto me-5">
+            <select className="form-select" id="inputGroupSelect01">
+              <option selected>All</option>
+              {arrayOrderByTeam.map((item, i) => (
+                <option value={i}>{item.team_name}</option>
+              ))}
+            </select>
+          </div>
+          <div className="col-auto">
+            <label for="inputPassword6" class="col-form-label">
+              Keywords:
+            </label>
+          </div>
+          <div className="col-auto">
+            <input
+              type="text"
+              id="inputPassword6"
+              className="form-control"
+              aria-describedby="passwordHelpInline"
+            ></input>
+          </div>
+        </div>
+        <div className="col-auto d-flex justify-content-center">
+          <button type="submit" className="btn btn-lg btn-secondary mt-5">
+            Search
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default LocalFileRead;
--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import "./sass/style.scss";
 // import reportWebVitals from './reportWebVitals';
 import Header from "./components/header";
 import Menu from "./components/menu";
-import Search from "./components/search";
 import ShowCharts from "./components/showCharts";
-import Table from "./components/table";
+// import Table from "./components/table";
 // import DatatablePage from './components/tableTwo';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "mdbreact/dist/css/mdb.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import DatatablePageTest from "./components/tableTest";
+import Search from "./components/search";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
