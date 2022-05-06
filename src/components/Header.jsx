673c7df172ec0fcdb3957798f3c35550e7165989
DILTPE - Ty Hsu <B0191@twusr.local>
2022-05-07 03:11:01 +0800

    add sticky-top

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import avatar from "../assets/avatar.png";
 
 function header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
       <div className="container-fluid">
         <a className="navbar-brand" href=".">
           <img src={logo} alt=""></img>
