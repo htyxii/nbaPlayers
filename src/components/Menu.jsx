d3f451c8417e88445593ab74eefec9518abc6003
DILTPE - Ty Hsu <B0191@twusr.local>
2022-05-07 11:38:56 +0800

    edit menu button

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,11 @@
 function header() {
   return (
     <nav aria-label="breadcrumb">
-      <ol className="breadcrumb bg-dark p-2">
+      <ol className="breadcrumb bg-dark p-0">
       <a className="navbar-brand" href=".">
-        <li className="breadcrumb-item active pl-5 text-warning" aria-current="page">
-          Player List
+        <li className="breadcrumb-item active pl-4 text-warning" aria-current="page">
+          <button className="btn btn-dark text-warning">Player List</button>
+          
         </li>
         </a>
       </ol>
